2a27d39efebcc8fc3c98e86eb9a9d0cc179defa0
ShowBaba <samwise858@gmail.com>
2021-04-01 08:24:28 +0100

    initial commit

---
+++ js/product.js
@@ -0,0 +1,27 @@
+const viewEntriesBtn = document.querySelector("#view-diary-btn");
+const container = document.querySelector(".entry-container");
+
+const getProduts = async () => {
+  const headers = new Headers();
+  let url = "http://localhost:3000/api/v1/product";
+  headers.append("Content-Type", "application/json");
+  try {
+    let response = await fetch(url, {
+      method: "GET",
+      headers: headers,
+    });
+    let json = await response.json();
+    console.log(json);
+    let products = json.products;
+    // console.log(`RESULT === ${products}`);
+    products.forEach((product) => {
+      let element = document.createElement("div");
+      element.className = "card diary-data";
+      element.innerHTML = JSON.stringify(product);
+      container.appendChild(element);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+window.onload = getProduts;
